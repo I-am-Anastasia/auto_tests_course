03ca40bcf312564a068ad03a885e46939d400263
Bezruchko Anastasia <anastasiabezruchko17@gmail.com>
2022-06-29 23:00:06 +0300

    task from course

---
+++ task.py
@@ -0,0 +1,25 @@
+
+from selenium import webdriver
+import time
+
+try: 
+    link = "http://suninjuly.github.io/file_input.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+    
+    input1 = browser.find_element_by_name("firstname")
+    input1.send_keys("Ivan")
+    input2 = browser.find_element_by_name("lastname")
+    input2.send_keys("Petrov")
+    input3 = browser.find_element_by_name("email")
+    input3.send_keys("sobaka")
+
+    # Отправляем заполненную форму
+    button = browser.find_element_by_tag_name("button")
+    button.click()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
